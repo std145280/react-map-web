26f3d24c04a2abd72e986dbb8f0da95e3255c901
std145280 <std145280@ac.eap.gr>
2020-12-14 00:42:17 +0200

    upload vehicle images for add-vahicles

--- src/components/AddVehicle.js
+++ src/components/AddVehicle.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import NavigationBar from "./NavigationBar";
 import db from '../firebase';
 
-export default function AddVehicle() {
+
+export default function AddVehicles() {
   const [title, setTitle] = useState('');
   const [type, setType] = useState('');
   const [passengers, setPassengers] = useState('');
@@ -11,6 +13,9 @@ export default function AddVehicle() {
   const [cph, setCPH] = useState('');
   const [place, setPlace] = useState('');
 
+  const [imageUrl, setImageUrl] = useState([]);
+
+  
   const handleOnChangeTITLE = (e) => {
     setTitle(e.target.value);
   };
@@ -33,6 +38,20 @@ export default function AddVehicle() {
     setPlace(e.target.value);
   };
 
+  const readImages = async (e) => {
+    const file = e.target.files[0];
+    const id = uuid();
+    const storageRef = db.storage().ref('images').child(id);
+    const imageRef = db.database().ref('images').child('daily').child(id);
+    await storageRef.put(file);
+    storageRef.getDownloadURL().then((url) => {
+      imageRef.set(url);
+      const newState = [...imageUrl, { id, url }];
+      setImageUrl(newState);
+    });
+  };
+
+
 
   const createVehicle = () => {
     var vehicleRef = db.database().ref('vehicles');
@@ -45,35 +64,45 @@ export default function AddVehicle() {
       cph,
       place,
       availableForRent: true,
+      imageUrl
     };
-
+    
     vehicleRef.push(vehicle);
   };
 
   return (
-        <>
-          <NavigationBar />
-          <div>
-            <form>
-                <h2 className="text-center">Import New Vehicle</h2>
+    <>
+      <NavigationBar />
+        <div>
+        <form>
+            <h2 className="text-center">Import New Vehicle</h2>
 
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeTITLE} value={title} placeholder="Title" /></div>
-
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeTYPE} value={type} placeholder="Type" /></div>
-
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangePASSENGERS} value={passengers} placeholder="Number of passengers" /></div>
-
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeFUEL} value={fuel} placeholder="Fuel" /></div>
-
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeYEAR} value={year} placeholder="Year" /></div>
-
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeCPH} value={cph} placeholder="Cost per hour" /></div>
-
-                <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangePLACE} value={place} placeholder="Place" /></div>
-
-                <div className="form-group"><button className="btn btn-primary" type="submit" onClick={createVehicle}> Submit </button></div>
-            </form>
-        </div> 
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeTITLE} value={title} placeholder="Title" /></div>
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeTYPE} value={type} placeholder="Type" /></div>
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangePASSENGERS} value={passengers} placeholder="Number of passengers" /></div>
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeFUEL} value={fuel} placeholder="Fuel" /></div>
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeYEAR} value={year} placeholder="Year" /></div>
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangeCPH} value={cph} placeholder="Cost per hour" /></div>
+            <div className="form-group"><input type="text" className="form-control" onChange={handleOnChangePLACE} value={place} placeholder="Place" /></div>
+            <center>
+              <div>
+                <h3>Upload Image</h3>
+                  <input type="file" accept="image/*" onChange={readImages} />
+                  {imageUrl
+                    ? imageUrl.map(({ id, url }) => {
+                        return (
+                          <div key={id}>
+                            <img src={url} alt="" />
+                          </div>
+                        );
+                      })
+                    : ''}
+                </div>
+              </center>
+            <div className="form-group"><button className="btn btn-primary" type="submit" onClick={createVehicle}> Submit </button>
+          </div>
+        </form>
+      </div> 
     </>
   );
 }
