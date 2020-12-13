db261ce6a250d598e1c7e4d537c68653ecac9116
std145280 <std145280@ac.eap.gr>
2020-12-14 00:00:06 +0200

    display-vehicles

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavigationBar from "./NavigationBar";
+import firebase from "../firebase";
+import VehicleList from "./control/VehicleList";
+import { CardDeck } from "react-bootstrap";
 
 export default function Vehicles() {
+  const [vehicleList, setVehicleList] = useState();
+
+  useEffect(() => {
+    const vehicleRef = firebase.database().ref("vehicles");
+    vehicleRef.on("value", (snapshot) => {
+      const vehicles = snapshot.val();
+      const vehicleList = [];
+      for (let id in vehicles) {
+        vehicleList.push({ id, ...vehicles[id] });
+      }
+      setVehicleList(vehicleList);
+    });
+  }, []);
+
   return (
     <>
       <NavigationBar />
-      All Vehicles
+      <div>
+        <h2> Available Vehicles </h2>
+
+        <CardDeck>
+          <center>
+            {vehicleList
+              ? vehicleList.map((vehicle, index) => (
+                  <VehicleList vehicle={vehicle} key={index} />
+                ))
+              : ""}
+          </center>
+        </CardDeck>
+      </div>
     </>
   );
 }
---
+++ src/components/control/VehicleList.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Card, Table } from "react-bootstrap";
+import firebase from "../../firebase";
+
+
+export default function VehicleList({ vehicle }) {
+  const deleteVehicle = () => {
+    const vehicleRef = firebase.database().ref("vehicles").child(vehicle.id);
+    vehicleRef.remove();
+  };
+  const completeVehicle = () => {
+    const vehicleRef = firebase.database().ref("vehicles").child(vehicle.id);
+    vehicleRef.update({
+      availableForRent: !vehicle.availableForRent,
+    });
+  };
+  return (
+    <div>
+      <h4 className={vehicle.complete ? "availableForRent" : ""}></h4>
+
+      <Card style={{ flex: 1 }}>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead></thead>
+            <tbody>
+              <tr>
+                <td colSpan="2">
+                  <b>
+                    <center>
+                      <Card.Img variant="top" src='imageCar' />
+                    </center>
+                  </b>
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2">
+                  <b>
+                    <Card.Title>
+                      <center>
+                        {" "}
+                        <h2>{vehicle.title}</h2>
+                      </center>
+                    </Card.Title>
+                  </b>
+                </td>
+              </tr>
+              <tr>
+                <td>Type: {vehicle.type}</td>
+                <td>Passengers #: {vehicle.passengers}</td>
+              </tr>
+              <tr>
+                <td>Fuel: {vehicle.fuel}</td>
+                <td>Year: {vehicle.year}</td>
+              </tr>
+              <tr>
+                <td>Cost/h: {vehicle.cph}</td>
+                <td>Place: {vehicle.place}</td>
+              </tr>
+              <tr>
+                <td colSpan="2">
+                  <h3>
+                    <center>
+                      The vehicle is{" "}
+                      <b>
+                        {vehicle.availableForRent
+                          ? "Available"
+                          : "Not Available"}
+                      </b>{" "}
+                      for rent.
+                    </center>
+                  </h3>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Card.Body>
+        <Card.Footer>
+          <center>
+            <button onClick={deleteVehicle}>
+              <b>Delete</b>
+            </button>
+            <button onClick={completeVehicle}>
+              <b>toggle availability</b>
+            </button>
+          </center>
+        </Card.Footer>
+      </Card>
+    </div>
+  );
+}
