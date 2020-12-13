c6a9c4481622bc14cedeee3353c1792a3e087371
Dim Skik <dimitrios.skikos@hotmail.com>
2020-12-13 22:15:11 +0200

    Added react-router links

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,21 @@ import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
 import UpdateProfile from "./UpdateProfile"
 
+import About from "./About";
+import AddPointOfInterest  from "./AddPointOfInterest";
+import AddTour  from "./AddTour";
+import AddTourGuide from "./AddTourGuide";
+import AddVehicle from "./AddVehicle";
+import PointOfInterest from "./PointOfInterest";
+import PointOfInterestForm from "./PointOfInterestForm";
+import Profile from "./Profile";
+import RentRequests from "./RentRequests";
+import Settings from "./Settings";
+import Tours from "./Tours";
+import TourGuides from "./TourGuides";
+import Vehicles from "./Vehicles";
+
+
 function App() {
   return (
     <Container
@@ -21,6 +36,21 @@ function App() {
             <Switch>
               <PrivateRoute exact path="/" component={Dashboard} />
               <PrivateRoute path="/update-profile" component={UpdateProfile} />
+
+              <PrivateRoute path="/About" component={About} />
+              <PrivateRoute path="/AddPointOfInterest" component={AddPointOfInterest} />
+              <PrivateRoute path="/AddTour" component={AddTour} />
+              <PrivateRoute path="/AddTourGuide" component={AddTourGuide} />
+              <PrivateRoute path="/AddVehicle" component={AddVehicle} />
+              <PrivateRoute path="/PointOfInterest" component={PointOfInterest} />
+              <PrivateRoute path="/PointOfInterestForm" component={PointOfInterestForm} />
+              <PrivateRoute path="/Profile" component={Profile} />
+              <PrivateRoute path="/RentRequests" component={RentRequests} />
+              <PrivateRoute path="/Settings" component={Settings} />
+              <PrivateRoute path="/TourGuides" component={TourGuides} />
+              <PrivateRoute path="/Tours" component={Tours} />
+              <PrivateRoute path="/Vehicles" component={Vehicles} />
+
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
