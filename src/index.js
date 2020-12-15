89301e7dd0e6fd14bad906142cd1aa31c9220aef
std145280 <std145280@ac.eap.gr>
2020-12-15 06:08:06 +0200

    install geocoder and change react-leaflet version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
 import "bootstrap/dist/css/bootstrap.min.css"
+import "leaflet-control-geocoder/dist/Control.Geocoder.js";
 import './styles.css'
 
 ReactDOM.render(
