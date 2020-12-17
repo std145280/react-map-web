49aeb1f8f09e8f8a7f844ae45fae7f724ebe7495
std138001 <72411523+std138001@users.noreply.github.com>
2020-12-17 23:34:38 +0200

    Basic Logic - choose from list

---
+++ src/components/control/ChoosePoi.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+
+const ChoosePois = () => {
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  const items = [
+    {
+      id: 1,
+      name: "Akropolis",
+      price: 20,
+    },
+    {
+      id: 2,
+      name: "Thisio",
+      price: 15,
+    },
+    {
+      id: 3,
+      name: "Sounio",
+      price: 10,
+    },
+  ];
+
+  useEffect(() => {
+    total();
+  }, [cart]);
+
+  const total = () => {
+    let totalVal = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalVal += cart[i].price;
+    }
+    setCartTotal(totalVal);
+  };
+
+  const addToCart = (el) => {
+      setCart([...cart, el]);
+  };
+
+  const removeFromCart = (el) => {
+    let hardCopy = [...cart];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
+    setCart(hardCopy);
+  };
+
+  const listItems = items.map((el) => (
+    <div key={el.id}>
+      {`${el.name}: $${el.price}`}
+      <input type="submit" value="add" onClick={() => addToCart(el)} />
+    </div>
+  ));
+
+  const cartItems = cart.map((el) => (
+    <div key={el.id}>
+      {`${el.name}: $${el.price}`}
+      <input type="submit" value="remove" onClick={() => removeFromCart(el)} />
+    </div>
+  ));
+
+  return (
+    <div>
+      STORE
+      <div>{listItems}</div>
+      <div>CART</div>
+      <div>{cartItems}</div>
+      <div>Total: ${cartTotal}</div>
+    </div>
+  );
+};
+
+export default ChoosePois;
