5fe460971d7c6aac1a085c54995880e3d421edaa
std138001 <72411523+std138001@users.noreply.github.com>
2020-12-17 23:38:45 +0200

    Basic logic - Choose item from list v2

--- src/components/control/ChoosePoi.js
+++ src/components/control/ChoosePoi.js
@@ -1,71 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+
+const items = [
+  {
+    id: 1,
+    name: "Akropolis",
+    price: 20,
+  },
+  {
+    id: 2,
+    name: "Thisio",
+    price: 15,
+  },
+  {
+    id: 3,
+    name: "Sounio",
+    price: 10,
+  },
+];
 
 const ChoosePois = () => {
-  const [cart, setCart] = useState([]);
-  const [cartTotal, setCartTotal] = useState(0);
-  const items = [
-    {
-      id: 1,
-      name: "Akropolis",
-      price: 20,
-    },
-    {
-      id: 2,
-      name: "Thisio",
-      price: 15,
-    },
-    {
-      id: 3,
-      name: "Sounio",
-      price: 10,
-    },
-  ];
+  const [cart, setCart] = React.useState([]);
+  const cartTotal = cart.reduce((total, { price = 0 }) => total + price, 0);
 
-  useEffect(() => {
-    total();
-  }, [cart]);
+  const addToCart = (item) => setCart((currentCart) => [...currentCart, item]);
 
-  const total = () => {
-    let totalVal = 0;
-    for (let i = 0; i < cart.length; i++) {
-      totalVal += cart[i].price;
-    }
-    setCartTotal(totalVal);
+  const removeFromCart = (item) => {
+    setCart((currentCart) => {
+      const indexOfItemToRemove = currentCart.findIndex((cartItem) => cartItem.id === item.id);
+
+      if (indexOfItemToRemove === -1) {
+        return currentCart;
+      }
+
+      return [
+        ...currentCart.slice(0, indexOfItemToRemove),
+        ...currentCart.slice(indexOfItemToRemove + 1),
+      ];
+    });
   };
 
-  const addToCart = (el) => {
-      setCart([...cart, el]);
-  };
+  const amountOfItems = (id) => cart.filter((item) => item.id === id).length;
 
-  const removeFromCart = (el) => {
-    let hardCopy = [...cart];
-    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-    setCart(hardCopy);
-  };
-
-  const listItems = items.map((el) => (
-    <div key={el.id}>
-      {`${el.name}: $${el.price}`}
-      <input type="submit" value="add" onClick={() => addToCart(el)} />
+  const listItemsToBuy = () => items.map((item) => (
+    <div key={item.id}>
+      {`${item.name}: $${item.price}`}
+      <button type="submit" onClick={() => addToCart(item)}>Add</button>
     </div>
   ));
 
-  const cartItems = cart.map((el) => (
-    <div key={el.id}>
-      {`${el.name}: $${el.price}`}
-      <input type="submit" value="remove" onClick={() => removeFromCart(el)} />
+  const listItemsInCart = () => items.map((item) => (
+    <div key={item.id}>
+      ({amountOfItems(item.id)} x ${item.price}) {`${item.name}`}
+      <button type="submit" onClick={() => removeFromCart(item)}>Remove</button>
     </div>
   ));
 
   return (
     <div>
       STORE
-      <div>{listItems}</div>
+      <div>{listItemsToBuy()}</div>
       <div>CART</div>
-      <div>{cartItems}</div>
+      <div>{listItemsInCart()}</div>
       <div>Total: ${cartTotal}</div>
+      <div>
+        <button onClick={() => setCart([])}>Clear</button>
+      </div>
     </div>
   );
 };
 
 export default ChoosePois;
+
+
