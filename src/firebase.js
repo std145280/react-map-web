b296db07e9a38be46090fb9fbef4cc7c3f593efd
std145280 <72468724+std145280@users.noreply.github.com>
2020-12-23 20:10:20 +0200

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwT8qCgvv7RKFkSMKfOe3zlMbgiw54uTM",
+  authDomain: "auth-development-7136f.firebaseapp.com",
+  databaseURL: "https://auth-development-7136f-default-rtdb.firebaseio.com",
+  projectId: "auth-development-7136f",
+  storageBucket: "auth-development-7136f.appspot.com",
+  messagingSenderId: "1044741522096",
+  appId: "1:1044741522096:web:2cac8bc4e2e33f6a37f175"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+
+
+export default firebase;  
